782391c329ffea622401fa1e99db46ea6cb10868
mattcbodily <mattcbodily@gmail.com>
2020-05-12 15:34:47 -0600

    added some structure to the Landing component

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import './Landing.scss';
 
 export default props => (
-    <div>Landing</div>
+    <div className='landing'>
+        <h1>App Name Here</h1>
+        <h3>Productivity Simplified</h3>
+        <button>Get Started</button>
+    </div>
 )
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Landing from './Components/Landing/Landing';
+import Login from './Components/Authentication/Login';
+import Register from './Components/Authentication/Register';
 
 export default (
     <Switch>
         <Route exact path='/' component={Landing} />
+        <Route path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
     </Switch>
 )
